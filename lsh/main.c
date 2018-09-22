5297fca24daf9fa4b7c3d8b9e493c5e89e72474e
Tal Livny <livnytal.dev@gmail.com>
2018-09-22 20:33:49 +0300

    Initial Commit

---
+++ lsh/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lsh
+//
+//  Created by Tal Livny on 22/09/2018.
+//  Copyright © 2018 Tal Livny. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
