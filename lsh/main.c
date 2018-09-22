77368cd432c7d9056ab4f443be5d9782026e504c
Tal Livny <livnytal.dev@gmail.com>
2018-09-23 01:04:50 +0300

    read line:

--- lsh/main.c
+++ lsh/main.c
@@ -7,9 +7,66 @@
 //
 
 #include <stdio.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+void lsh_loop(void);
+
+int main(int argc, char **argv) {
+    lsh_loop();
+    
+    return EXIT_SUCCESS;
+}
+
+#define LSH_RL_BUFSIZE 1024
+char *lsh_read_line(void) {
+    int bufsize = LSH_RL_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * bufsize);
+    int c;
+    
+    if (!buffer) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    while (1) {
+        c = getchar();
+        
+        if (c == EOF || c == '\n') {
+            buffer[position] = '\0';
+            return buffer;
+        } else {
+            buffer[position] = c;
+        }
+        position++;
+        
+        if (position >= bufsize) {
+            bufsize += LSH_RL_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+            if (!buffer) {
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
+void lsh_loop(void) {
+    char *line;
+    char **args;
+    int status;
+    
+    do {
+        printf("lsh>");
+        line = lsh_read_line();
+        args = lsh_split_line(line);
+        status = lsh_execute(args);
+        
+        free(line);
+        free(args);
+    } while (status);
 }
