7ddce44768fbf6f8d8a9ab89a35284eeabc173d3
Tal Livny <livnytal.dev@gmail.com>
2018-09-27 01:57:38 +0300

    enough for today

--- lsh/main.c
+++ lsh/main.c
@@ -16,10 +16,49 @@
 
 void lsh_loop(void);
 
-int main(int argc, char **argv) {
-    lsh_loop();
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+
+char *builtin_str[] = {
+    "cd",
+    "help",
+    "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+    &lsh_cd,
+    &lsh_help,
+    &lsh_exit,
+};
+
+int lsh_num_builtins() {
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+int lsh_cd(char **args) {
+    if (args[1] == NULL) {
+        fprintf(stderr,"lsh: expected argument to \"cd\"\n");
+    } else {
+        if (chdir(args[1]) != 0) {
+            perror("lsh");
+        }
+    }
+    return 1;
+}
+
+int lsh_help(char **args) {
+    int i;
+    printf("LSH\n");
+    printf("Type program names and arguments, and hit enter.\n");
+    printf("The following are built in:\n");
     
-    return EXIT_SUCCESS;
+    for (i = 0; i < lsh_num_builtins(); i++) {
+        printf("  %s\n", builtin_str[i]);
+    }
+    
+    printf("Use the man command for information on other programs.\n");
+    return 1;
 }
 
 #define LSH_RL_BUFSIZE 1024
@@ -95,6 +134,29 @@ char **lsh_split_line(char *line) {
     
 }
 
+int lsh_launch(char **args) {
+    pid_t pid, wpid;
+    int status;
+    
+    pid = fork();
+    if (pid == 0) {
+        if (execvp(args[0], args) == -1) {
+            perror("lsh");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+        perror("lsh");
+    } else {
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    
+    return 1;
+}
+
+
+
 void lsh_loop(void) {
     
     char *line;
@@ -110,4 +172,10 @@ void lsh_loop(void) {
         free(line);
         free(args);
     } while (status);
+}
+
+int main(int argc, char **argv) {
+    lsh_loop();
+    
+    return EXIT_SUCCESS;
 }
