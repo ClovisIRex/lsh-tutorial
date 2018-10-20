1ba191e0cd7c801710ac3a3e3cdc7db90b062e88
Tal Livny <tal@totango.com>
2018-10-20 18:26:48 +0300

    finished tutorial

--- lsh/main.c
+++ lsh/main.c
@@ -36,6 +36,10 @@ int lsh_num_builtins() {
     return sizeof(builtin_str) / sizeof(char *);
 }
 
+int lsh_exit(char **args) {
+    return 0;
+}
+
 int lsh_cd(char **args) {
     if (args[1] == NULL) {
         fprintf(stderr,"lsh: expected argument to \"cd\"\n");
@@ -155,6 +159,22 @@ int lsh_launch(char **args) {
     return 1;
 }
 
+int lsh_execute(char **args) {
+    
+    int i;
+    
+    if (args[0] == NULL) {
+        return 1;
+    }
+    
+    for (i=0; i < lsh_num_builtins(); i++) {
+        if (strcmp(args[0], builtin_str[i]) == 0) {
+            return (*builtin_func[i])(args);
+        }
+    }
+    
+    return lsh_launch(args);
+}
 
 
 void lsh_loop(void) {
